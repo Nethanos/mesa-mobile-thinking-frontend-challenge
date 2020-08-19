6b30100734d6eea0247f86e8d9c568e8d5791f31
AdeildoNeto <n.santos1897@gmail.com>
2020-08-18 21:40:00 -0300

    [INITIAL COMMIT] Creating project scaffold

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './pages/home/home.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
+import { UserFormComponent } from './components/user-form/user-form.component';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +18,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule,
+    HttpClientModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot({
+      timeOut: 5000,
+      preventDuplicates: true,
+      positionClass: 'inline'
+    }
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]

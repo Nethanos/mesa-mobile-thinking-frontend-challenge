a750adfd8cb93f1eaceb9d6c8b973b1a0313d9c6
AdeildoNeto <n.santos1897@gmail.com>
2020-08-19 16:56:39 -0300

    [REFACTOR] Adjusting home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,22 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { LoginInformation } from '../../model/login-information';
 import { ToastrService } from 'ngx-toastr';
 import { FormBuilder } from '@angular/forms';
 import * as M from 'materialize-css';
-
-
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor(private authService: AuthService, private toaster: ToastrService, private formBuilder: FormBuilder) {
+  constructor(private authService: AuthService, private toaster: ToastrService, private formBuilder: FormBuilder,
+    private router: Router) {
   }
+
+  modalInstance: any;
 
 
   loginForm = this.formBuilder.group({
@@ -28,18 +29,31 @@ export class HomeComponent implements OnInit {
     this.initializeModalListener();
   }
 
+  ngOnDestroy(): void {
+    this.closeModal();
+  }
+
   initializeModalListener(): void {
-    document.addEventListener('DOMContentLoaded', function () {
-      var elems = document.querySelectorAll('.modal');
-      var instances = M.Modal.init(elems, {});
+    document.addEventListener('DOMContentLoaded', () => {
+      const elem = document.querySelector('.modal');
+
+      M.Modal.init(elem);
     });
+  }
+
+  closeModal(): void {
+    const modal = M.Modal.getInstance(document.querySelector('.modal'));
+    modal.destroy();
   }
 
 
   loginOnApplication(): void {
     const loginInformation = { ...this.loginForm.value } as LoginInformation;
 
-    this.authService.login(loginInformation).subscribe((response) => { }, () => {
+    this.authService.login(loginInformation).subscribe(response => {
+      sessionStorage.setItem("userToken", response.token);
+      this.goToUserProfile("1");
+    }, () => {
       this.toaster.error("Por favor, verifique suas credenciais!", "Login incorreto!")
     })
   }
@@ -47,6 +61,11 @@ export class HomeComponent implements OnInit {
 
   displaySignupError(errorMessage: string): void {
     this.toaster.error(errorMessage, "Erro ao tentar registro!");
+  }
+
+
+  goToUserProfile(event: string) {
+    this.router.navigate([`user/profile/${event}`])
   }
 
 }

fe0c65191d5b5aa6be10d06e2f9d6f8fe6e0612a
AdeildoNeto <n.santos1897@gmail.com>
2020-08-20 14:10:00 -0300

    [REFACTOR] Refactor on external scripts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignInComponent } from './pages/sign-in/signIn.component';
 import { HomeComponent } from './pages/home/home.component';
 
 
 const routes: Routes = [
-  { path: "", component: HomeComponent, pathMatch: "full" },
+  { path: "", component: SignInComponent, pathMatch: "full" },
+  { path: "home", component: HomeComponent },
   { path: "user", loadChildren: () => import('./pages/user/user.module').then(m => m.UserModule) }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeModule } from './pages/home/home.module';
+import { SignInModule } from './pages/sign-in/signIn.module';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { ToastrModule } from 'ngx-toastr';
+import { HomeComponent } from './pages/home/home.component';
+import { SharedModule } from './shared/shared.module';
 
 
 
 
 @NgModule({
   declarations: [
-    AppComponent],
+    AppComponent,
+    HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HomeModule,
+    SignInModule,
+    SharedModule,
     HttpClientModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot({

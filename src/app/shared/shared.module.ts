2ac21a9b6de37b7e5d7cb38238d6ecc701d0f739
AdeildoNeto <n.santos1897@gmail.com>
2020-08-20 14:16:26 -0300

    [FEATURE] Adding new components to sharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,18 +3,21 @@ import { CommonModule } from '@angular/common';
 import { MapComponent } from '../components/map/map.component';
 import { environment } from '../../environments/environment';
 import { AgmCoreModule } from '@agm/core';
+import { NavbarComponent } from './../components/navbar/navbar.component';
+import { RatingComponent } from './../components/rating/rating.component';
+
 
 
 
 
 @NgModule({
-  declarations: [MapComponent],
+  declarations: [MapComponent, NavbarComponent, RatingComponent],
   imports: [
     CommonModule,
     AgmCoreModule.forRoot({
       apiKey: environment.placesApi
     }),
   ],
-  exports: [MapComponent]
+  exports: [MapComponent, NavbarComponent]
 })
 export class SharedModule { }

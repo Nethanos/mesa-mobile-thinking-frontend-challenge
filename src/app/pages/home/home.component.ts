95dc71c23e144c818bbdde44e1d2f1744570619d
AdeildoNeto <n.santos1897@gmail.com>
2020-08-20 14:11:33 -0300

    [FEATURE][WIP] Creating home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,71 +1,15 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Validators } from '@angular/forms';
-import { AuthService } from '../../services/auth.service';
-import { LoginInformation } from '../../model/login-information';
-import { ToastrService } from 'ngx-toastr';
-import { FormBuilder } from '@angular/forms';
-import * as M from 'materialize-css';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit, OnDestroy {
+export class HomeComponent implements OnInit {
 
-  constructor(private authService: AuthService, private toaster: ToastrService, private formBuilder: FormBuilder,
-    private router: Router) {
-  }
-
-  modalInstance: any;
-
-
-  loginForm = this.formBuilder.group({
-    email: ['', Validators.compose([Validators.required, Validators.email])],
-    password: ['', Validators.required]
-  });
+  constructor() { }
 
   ngOnInit(): void {
-    this.initializeModalListener();
-  }
-
-  ngOnDestroy(): void {
-    this.closeModal();
-  }
-
-  initializeModalListener(): void {
-    document.addEventListener('DOMContentLoaded', () => {
-      const elem = document.querySelector('.modal');
-
-      M.Modal.init(elem);
-    });
-  }
-
-  closeModal(): void {
-    const modal = M.Modal.getInstance(document.querySelector('.modal'));
-    modal.destroy();
-  }
-
-
-  loginOnApplication(): void {
-    const loginInformation = { ...this.loginForm.value } as LoginInformation;
-
-    this.authService.login(loginInformation).subscribe(response => {
-      sessionStorage.setItem("userToken", response.token);
-      this.goToUserProfile("1");
-    }, () => {
-      this.toaster.error("Por favor, verifique suas credenciais!", "Login incorreto!")
-    })
-  }
-
-
-  displaySignupError(errorMessage: string): void {
-    this.toaster.error(errorMessage, "Erro ao tentar registro!");
-  }
-
-
-  goToUserProfile(event: string) {
-    this.router.navigate([`user/profile/${event}`])
   }
 
 }

2967f8880da5b5aa72861e0fd1451d27d9a45409
AdeildoNeto <n.santos1897@gmail.com>
2020-08-22 20:44:22 -0300

    [REFACTOR] Removed unnecessary components in shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,23 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MapComponent } from '../components/map/map.component';
-import { environment } from '../../environments/environment';
-import { AgmCoreModule } from '@agm/core';
 import { NavbarComponent } from './../components/navbar/navbar.component';
-import { RatingComponent } from './../components/rating/rating.component';
-import { PlaceSectionComponent } from '../components/place-section/place-section.component';
-import { CommentSectionComponent } from '../components/comment-section/comment-section.component';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
-const Components = [MapComponent, NavbarComponent, RatingComponent, PlaceSectionComponent, CommentSectionComponent]
+const Components = [NavbarComponent]
 @NgModule({
   declarations: Components,
   imports: [
     CommonModule,
     FormsModule,
-    AgmCoreModule.forRoot({
-      apiKey: environment.placesApi
-    }),
+    RouterModule
   ],
   exports: Components
 })

c7b31b654d82dc4636a3a349ad8f4ee471f8c284
AdeildoNeto <n.santos1897@gmail.com>
2020-08-22 20:42:29 -0300

    [FEATURE] Added links in navbar component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageManager } from '../../middlewares/local-storage-manager';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private localStorageManager: LocalStorageManager) { }
 
   ngOnInit(): void {
+  }
+
+
+  goToProfile() {
+    const user = this.localStorageManager.getUser();
+
+    return [`/profile/${user.id}`]
   }
 
 }

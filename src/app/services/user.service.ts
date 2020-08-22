d2faba8e0cd8e33f9d290329cdb3cf20598c09ea
AdeildoNeto <n.santos1897@gmail.com>
2020-08-22 20:44:05 -0300

    [FEATURE] Using abstract service in users service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,25 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { LoginInformation } from '../model/login-information';
 import { Observable } from 'rxjs';
 import { User } from '../model/user';
-
+import { AbstractService } from './abstract.service';
 @Injectable({
     providedIn: 'root'
 })
-export class UserService {
+export class UserService extends AbstractService {
 
-    constructor(private http: HttpClient) { }
-
-    API_URL = 'https://reqres.in/api/users'
-
-
-    retrieve(id: string): Observable<any> {
-        return this.http.get<any>(`${this.API_URL}/${id}`);
+    constructor(http: HttpClient) {
+        super(http, 'users');
     }
-
-
-    update(user: User): Observable<any> {
-        return this.http.put<any>(`${this.API_URL}/${user.id}`, user);
-    }
-
 
 }
